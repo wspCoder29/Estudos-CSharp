857134d91a33edffef976d1fa3dc4d036be7ebd6
Leandro Ferreira <86746732+wspCoder29@users.noreply.github.com>
2022-04-12 15:58:08 -0300

    Add files via upload

---
+++ Calculo Imposto/Entities/COFINS.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMPOSTO.Entities
+{
+    class COFINS : IImposto
+
+    {
+        public double Valor { get; set; }
+
+        public COFINS(double valor)
+        {
+            Valor = valor;
+        }
+
+        public double CalcularImposto()
+        {
+            double calcValorImposto = 0;
+            // Cofins - Regra: 
+            //se valor > 17000 - imposto de 8%
+
+            if (Valor > 17000)
+            {
+                calcValorImposto = calcValorImposto * 0.08;
+            }
+            else
+            {
+                calcValorImposto = 0;
+            }
+            return calcValorImposto;
+        }
+
+
+        
+
+
+
+    }
+}
