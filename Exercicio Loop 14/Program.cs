66df50870616881e943704c2efd291332c830c9f
Leandro Ferreira <86746732+wspCoder29@users.noreply.github.com>
2022-04-13 10:29:05 -0300

    Add files via upload

---
+++ Exercicio Loop 14/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace FIBO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+
+
+            //  1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584
+
+
+            //GERADOR DE FIBONACCI
+            //Verifica se item esta dentro da sequencia de Fibonacci
+            int enesimoItem = 20;
+            bool test = false;
+
+            //Procurar
+            int alvo = 55;
+
+            int y = 1, x = 0;
+            Console.Write(x + " " + y + " ");
+            for (int i = 0; i < enesimoItem - 2; i++)
+            {
+                y = y + x;
+                x = y - x;
+                Console.Write(y + " ");
+
+                if (y==alvo)
+                {
+                    test = true;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(alvo+" "+test);
+
+
+
+
+        }
+    }
+}
