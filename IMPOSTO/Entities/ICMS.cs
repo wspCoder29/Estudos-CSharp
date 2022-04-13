e57f352270018cbf2e80ad0606b1d71ae3c4a4da
Leandro Ferreira <86746732+wspCoder29@users.noreply.github.com>
2022-04-13 10:43:22 -0300

    Add files via upload

---
+++ IMPOSTO/Entities/ICMS.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMPOSTO.Entities
+{
+
+    class ICMS : IImposto
+    {
+
+        public double Valor { get; set; }
+
+        public ICMS(double valor)
+        {
+            Valor = valor;
+        }
+
+        public double CalcularImposto()
+        {
+            return Valor * 0.03;
+        }
+
+        
+
+
+
+
+    }
+}
