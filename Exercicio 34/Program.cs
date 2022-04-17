1b8c39195bbdbd10fa803095c61a2a61a4cf9b19
Leandro Ferreira <86746732+wspCoder29@users.noreply.github.com>
2022-04-17 18:16:17 -0300

    Intervalos

---
+++ Exercicio 34/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TESTE7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o multiplicador: ");
+            int m = int.Parse(Console.ReadLine());
+
+            Console.Write("Início do intervalo: ");
+            int inicioIntervalo = int.Parse(Console.ReadLine());
+
+            Console.Write("Fim do intervalo: ");
+            int fimIntervalo = int.Parse(Console.ReadLine());
+
+            int resultado = 0;
+
+
+            for (int i = inicioIntervalo; i <= fimIntervalo; i++)
+            {
+                if (m <= 1000 || inicioIntervalo > fimIntervalo || inicioIntervalo < 0 || fimIntervalo < 0)
+                {
+                    resultado = m * i;
+                }
+                Console.WriteLine($"{m} x {i} = " + resultado);
+
+
+
+            }
+        }
+    }
+}
