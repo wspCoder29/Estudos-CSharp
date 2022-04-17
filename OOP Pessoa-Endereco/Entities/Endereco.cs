cfc76bb52b43a7934fa3e4d9da1229d1b607ab5c
Leandro Ferreira <86746732+wspCoder29@users.noreply.github.com>
2022-04-16 23:11:03 -0300

    Add files via upload

---
+++ OOP Pessoa-Endereco/Entities/Endereco.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PESSOA_ENDERECO.Entities
+{
+    class Endereco
+    {
+
+        public string Rua { get; private set; }
+
+        public int Numero { get; private set; }
+
+        public string CEP { get; private set; }
+
+        public string Tipo { get; private set; }
+
+
+        public Endereco() { }
+
+        public Endereco(string rua, int numero, string cEP, string tipo)
+        {
+            Rua = rua;
+            Numero = numero;
+            CEP = cEP;
+            Tipo = tipo;
+        }
+
+
+        public override string ToString()
+        {
+            string str = "";
+            str += $"{Rua}, Número: {Numero}, CEP: {CEP}, Tipo: {Tipo}";
+            return str;
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ OOP Pessoa-Endereco/Entities/Pessoa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PESSOA_ENDERECO.Entities
+{
+    class Pessoa
+    {
+
+        public string Nome { get; private set; }
+        public Endereco Endereco { get; private set; }
+        public string Telefone { get; private set; }
+
+        public Pessoa() { }
+
+        public Pessoa(string nome, Endereco endereco, string telefone)
+        {
+            Nome = nome;
+            Endereco = endereco;
+            Telefone = telefone;
+        }
+
+
+        public override string ToString()
+        {
+            string str = "";
+            str += $"{Nome}, {Endereco}, Tel: {Telefone}" + "\n";
+            return str;
+        }
+
+
+
+
+    }
+}
