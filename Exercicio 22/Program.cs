312653b01b596c6ec488cdefc1db5748c050359c
Leandro Ferreira <86746732+wspCoder29@users.noreply.github.com>
2022-04-17 18:07:41 -0300

    Add files via upload

---
+++ Exercicio 22/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace ZTEST4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Calcula a expectativa de vida
+
+            string dataNasc = "01/01/1990";
+            string genero = "F";
+
+            DateTime dtAtual = DateTime.Now;
+
+            DateTime dtNasc = DateTime.ParseExact(dataNasc, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+            int idade = dtAtual.Subtract(dtNasc).Days / 365;
+
+            Console.WriteLine("Idade: " + idade);
+
+            int expectativaDeVida = 0;
+            if (genero == "M")
+            {
+                expectativaDeVida = 80 - idade;
+
+            }
+            else if (genero == "F")
+            {
+                expectativaDeVida = 90 - idade;
+            }
+
+            Console.WriteLine($"Expectativa de vida: {expectativaDeVida} anos");
+
+
+
+
+
+        }
+    }
+}
