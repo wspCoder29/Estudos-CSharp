a7f0ed53905845827eba46c92f293a99f62f7d12
Leandro Ferreira <86746732+wspCoder29@users.noreply.github.com>
2022-04-18 09:30:23 -0300

    Add files via upload

---
+++ Exercicio 11/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace ZTEST6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite num de 4 dígitos - 1000 e 9999");
+
+            bool control = true;
+            int input = 0;
+
+            while (control)
+            {
+                if (input <= 999 | input > 9999)
+                {
+                    Console.Write("Num: ");
+                    input = int.Parse(Console.ReadLine());
+                }
+
+                if (input <= 999 | input > 9999)
+                {
+                    Console.WriteLine("Deve estar entre 1000 e 9999");
+                }
+                else
+                {
+                    control = false;
+                }
+            }
+
+            string converteInput = input.ToString();
+
+            char[] c = converteInput.ToCharArray();
+
+            int[] v = Array.ConvertAll(c, x => (int)Char.GetNumericValue(x));
+
+            int totalPares = v.Where(x => x % 2 == 0).Count();
+
+            Console.WriteLine("Total pares: " + totalPares);
+
+
+
+
+        }
+    }
+}
