384c9a8a134af5393055acfba164914eff104927
Leandro Ferreira <86746732+wspCoder29@users.noreply.github.com>
2022-05-02 19:04:31 -0300

    Testando LINQ com ThenBy()

---
+++ OrderBY_ThenBy/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderBY_ThenBy.Entities
+{
+    class User
+    {
+        public string Name { get; set; }
+        public string Mail { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            string str = "";
+            str += $"{Name} | {Mail} | {Age}"+"\n";
+
+            return str;
+        }
+
+
+    }
+}
---
+++ OrderBY_ThenBy/Program.cs
@@ -0,0 +1,46 @@
+﻿using OrderBY_ThenBy.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OrderBY_ThenBy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://csharp.net-tutorials.com/pt/439/linq/organizando-dados-os-metodos-orderby-thenby/
+
+            List<User> li = new List<User>
+            {
+
+                new User() { Name = "John Doe", Mail = "john@doe.com", Age = 42 },
+                new User() { Name = "Jane Doe", Mail = "jane@doe.com", Age = 42 },
+                new User() { Name = "Joe Doe", Mail = "joe@doe.com", Age = 8 },
+                new User() { Name = "Jenna Doe", Mail = "another@doe.com", Age = 8 },
+
+            };
+
+            Console.WriteLine(" " + String.Join(" ", li));
+            Console.WriteLine();
+
+            li = li.OrderBy(x => x.Age).ToList();
+
+            Console.WriteLine(" " + String.Join(" ", li));
+
+            li = li.OrderBy(x => x.Age).ThenBy(x => x.Name).ToList();
+
+
+            Console.WriteLine(" " + String.Join(" ", li));
+
+
+
+        }
+
+
+
+
+
+
+    }
+}
